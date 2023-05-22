ef3f166999bfaaf5e565f5a90120c25f17694caa
guga2-6-4 <106117352+guga2-6-4@users.noreply.github.com>
2023-05-22 18:42:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function draw() {
  fill(255,0,0);
  //stroke(255,0,0);
  //circle(noseX, noseY, 20);
- image(clownNose, noseX, noseY, 100, 10);
+ image(clownNose, noseX, noseY, 130, 10);
 }
 
 function takeSnapshot(){
